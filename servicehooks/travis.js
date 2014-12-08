d09b94b79e8daa53a1afcc6f21a6e13ab0c3509a
Neelabh Gupta <gupta.neelabh@gmail.com>
2014-12-07 22:22:26 -0600

    Travis CI notification hook integration: debug parsing of payload

--- servicehooks/travis.js
+++ servicehooks/travis.js
@@ -5,19 +5,22 @@ var bot = require('../bot.js');
 
 module.exports = function (registerRoute) {
   registerRoute('post', '/travisci', function (headers, requestData) {
+    console.log('================== Travis CI request headers:', headers);
+    console.log('================== Travis CI request data:', requestData);
+
     // excellent article: http://blog.frankgrimm.net/2010/11/howto-access-http-message-body-post-data-in-node-js/
     var requestBody = querystring.parse(requestData);
     var payload;
     try {
       payload = JSON.parse(requestBody.payload);
     } catch (e) {
-      console.log('Travis hook error while parsing JSON: Travis CI notification data: \'%s\', Error:', requestBody, e);
+      console.log('Travis hook error while parsing JSON:', e);
+      console.log('Travis CI notification hook request body:', requestBody);
       return;
     }
     if (!payload) {
       return;
     }
     console.log(payload);
-    console.log(headers);
   });
 };
