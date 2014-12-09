ef52e64ce107cc33c46904e4c409ad18c9e525b8
Neelabh Gupta <gupta.neelabh@gmail.com>
2014-12-09 09:03:36 -0600

    Parse Travis CI payload and send status message to chat group

--- src/servicehooks/travis.js
+++ src/servicehooks/travis.js
@@ -3,10 +3,22 @@
 var bot = require('../bot');
 var config = require('../config');
 var crypto = require('crypto');
+var util = require('util');
 
 var isValidRequest = function (headers) {
   var digest = crypto.createHash('sha256').update(headers['travis-repo-slug'] + config.travisUserToken).digest('hex');
   return digest === headers['authorization'];
+};
+
+var getChatMessageText = function (payload) {
+  var commit_details;
+  if (payload.type === 'pull_request') {
+    commit_details = util.format('pull-request #%d : %s', payload.pull_request_number, payload.author_name);
+  } else {
+    commit_details = util.format('%s - %s : %s', payload.branch, payload.commit, payload.author_name);
+  }
+  return util.format('Travis CI - %s - build #%d (%s): %s.\nBuild url: %s',
+    payload.repository.name, payload.number, commit_details, payload.status_message, payload.build_url);
 };
 
 module.exports = function (registerRoute) {
@@ -32,6 +44,6 @@ module.exports = function (registerRoute) {
       return;
     }
 
-    console.log(payload);
+    bot.postMessageWithGroupLocalID('1', getChatMessageText(payload));
   });
 };
